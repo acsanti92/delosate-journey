94b367052d3b3a67d06d724485154e03ac09cf86
acsanti92 <armando.santiago@pluto.tv>
2023-04-17 12:19:00 -0700

    Input Player Character Controller and Movements

--- des-jour/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ des-jour/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -21,8 +21,13 @@ public class PlayerTestState : PlayerBaseState
         movement.x = stateMachine.InputReader.MovementValue.x;
         movement.y = 0;
         movement.z = stateMachine.InputReader.MovementValue.y;
-        // Translate the player by the movement Vector3
-        stateMachine.transform.Translate(movement * deltaTime);
+        // Move the CharacterController by the movement Vector3 multiplied by the deltaTime and the FreeLookMovementSpeed
+        stateMachine.Controller.Move(movement * deltaTime * stateMachine.FreeLookMovementSpeed);
+
+        // If the InputReader's MovementValue is equal to Vector2.zero, return
+        if (stateMachine.InputReader.MovementValue == Vector2.zero) { return; }
+        // Set the rotation of the CharacterController to the rotation of the Quaternion.LookRotation of the movement Vector3
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
     }
 
     public override void Exit()
